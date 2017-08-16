7f6b4487798f13e8ac991dd0fdfab382e07b69c0
Patrick Schlaepfer <patrick.schlaepfer@ic-consult.ch>
2017-08-16 07:44:35 +0200

    Added REST backend

--- frontend/src/app/identities/identity-list.component.ts
+++ frontend/src/app/identities/identity-list.component.ts
@@ -1,24 +1,11 @@
 interface IIdentityListController {
-    vehicles: any;
-    setVehicles(): void;
+    getIdentities():void;
 }
 
 export class IdentityListController implements IIdentityListController {
   static componentName: string = 'IdentityListController';
 
-    vehicles: any;
-
-    static $inject = ['IdentitiesService'];
-    constructor(private vehicleMakeService: IIdentitiesService) {
-        this.vehicles = {};
-        this.setVehicles();
-    }
-
-    setVehicles(): void {
-        this.vehicleMakeService.getVehicles().then((data) => {
-        this.vehicles = data;
-        console.log(this.vehicles);
-        });
-    }
+  static $inject: Array<string> = ['$log', IdentitiesService.serviceName]
+  
 
 }
